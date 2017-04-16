48c0eecd27f9045698330efe18ca375d440b66e1
Andrea Parodi <andrea@parro.it>
2017-04-16 18:21:39 +0200

    iterateFunction

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
-export default function iterateFun() {
-	return 42;
+export function iterateFunction(fn, source) {
+	// Dconsole.log({source: typeof source[Symbol.iterator]})
+	if (typeof source[Symbol.iterator] === 'function') {
+		const sourceIterator = source[Symbol.iterator];
+		source = sourceIterator.call(source);
+	}
+
+	return {
+		[Symbol.iterator]: () => ({
+			next() {
+				return fn(source.next.bind(source));
+			}
+		})
+	};
 }
--- test.js
+++ test.js
@@ -1,6 +1,12 @@
 import test from 'ava';
-import iterateFun from '.';
+import {iterateFunction} from '.';
 
 test('exports a function', t => {
-	t.is(typeof iterateFun, 'function');
+	const arr = [42, 43, 44];
+	const result = iterateFunction(next => {
+		const res = next();
+		res.value = Number(res.value) + 1;
+		return res;
+	}, arr);
+	t.deepEqual(Array.from(result), [43, 44, 45]);
 });
