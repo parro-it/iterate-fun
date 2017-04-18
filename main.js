09fc25eeacfc3c3e89456d48a4e0ed042a17b496
Andrea Parodi <andrea@parro.it>
2017-04-18 21:47:33 +0200

    readme docs

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
 export const Done = {};
 
 export const None = {};
-
+/**
+ *	Return an iterable that continously call `fn` argument,
+ *	iterating over the results.
+ *
+ *	@param {Function} fn the function that returns result iterable items.
+ *	@return A new iterable that iterate over `fn` results.
+ *	@example
+ *	const arr = [42, 43, 44];
+ *	const result = iterateFunction(() => {
+ *		if (arr.length === 0) {
+ *			return Done;
+ *		}
+ *		return arr.shift() + 1;
+ *	});
+ *	// results === [43, 44, 45]
+ */
 export function iterateFunction(fn) {
 	function next() {
 		const value = fn();
