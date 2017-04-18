7f4b9b08fdbe6bacec0ac2c54b01f860f4dbdb28
Andrea Parodi <andrea@parro.it>
2017-04-18 21:26:49 +0200

    tests

--- test.js
+++ test.js
@@ -1,7 +1,9 @@
 import test from 'ava';
-import {iterateFunction, Done} from '.';
+import {iterateFunction, Done, None} from '.';
 
-test('exports a function', t => {
+const isEven = value => value % 2 === 0;
+
+test('return an iteration of fn results', t => {
 	const arr = [42, 43, 44];
 	const result = iterateFunction(() => {
 		if (arr.length === 0) {
@@ -10,4 +12,29 @@ test('exports a function', t => {
 		return arr.shift() + 1;
 	});
 	t.deepEqual(Array.from(result), [43, 44, 45]);
+});
+
+test('done stop the iteration', t => {
+	const arr = [42, 43, 44];
+	const result = iterateFunction(() => {
+		if (arr.length === 2) {
+			return Done;
+		}
+		return arr.shift();
+	});
+	t.deepEqual(Array.from(result), [42]);
+});
+
+test('None results are skipped', t => {
+	const arr = [42, 43, 44];
+	const result = iterateFunction(() => {
+		if (arr.length === 0) {
+			return Done;
+		}
+
+		const value = arr.shift();
+
+		return isEven(value) ? value : None;
+	});
+	t.deepEqual(Array.from(result), [42, 44]);
 });
